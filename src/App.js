33fc7d86b946ef87b04e89dfe4aa9a5b88bcc461
Jeffrey <jeffrey.frani@gmail.com>
2023-05-01 00:54:11 -0700

    Setting up Semantic Structure

--- src/App.js
+++ src/App.js
@@ -2,10 +2,40 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Home
-    </div>
+    <html>
+      <head>
+        <img src="logo.png">
+      </head>
+      <body>
+        <header>
+
+        </header>
+        <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="menu.html">Menu</a></li>
+            <li><a href="reservations.html">Reservations</a></li>
+            <li><a href="orderonline.html">Order Online</a></li>
+            <li><a href="login.html">Login</a></li>
+        </ul>
+        </nav>
+        <main>
+
+        </main>
+        <footer>
+        <div>
+            <img src="logo_footer.png">
+        </div>
+        <div>
+            <p>Copyright Little Lemon</p>
+        </div>
+        </footer>
+      </body>
+    </html>
   );
 }
 
 export default App;
+
+
